1659accd7d234f5c8e91fc797560f35821a34bb8
Diksha4248 <88928334+Diksha4248@users.noreply.github.com>
2023-10-20 00:45:49 +0530

    added problem statement as a comment in evenodd code

--- SeggregateEvenOdds.cpp
+++ SeggregateEvenOdds.cpp
@@ -1,4 +1,5 @@
-
+// Given a Linked List of integers, write a function to modify the linked list such that all even numbers appear before all 
+// the odd numbers in the modified linked list. Also, keep the order of even and odd numbers the same.
 
 #include <bits/stdc++.h>
 using namespace std;
